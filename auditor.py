f58c134da381f4eb92ecb4d7aa912a7d60e25b41
Yuan Yee <2603843@sit.singaporetech.edu.sg>
2026-09-10 15:22:41 +0800

    Allowed users to both view their current stock amount and enter their stock

--- auditor.py
+++ auditor.py
@@ -3,9 +3,16 @@ inventory = 0
 
 while True:
     print ("===================")
+    print ("Please enter 2 to view current inventory amount")
     print ("Please enter 1 to input your stock quantity")
     print ("Please enter 0 to exit the program")
-    choice = int(input("Enter your Choice here    "))
+    choice = int(input("Enter your Choice here  "))
+    if choice == 2:
+        print (inventory)
+
+    if choice == 1:
+        stock = int(input("Please enter the stock amount"))
+        inventory = inventory + stock
 
     if choice == 0:
         break

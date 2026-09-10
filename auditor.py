0a68ea424c3393bfe95a19e6a1ff74dc9701a385
Yuan Yee <2603843@sit.singaporetech.edu.sg>
2026-09-10 15:34:41 +0800

    Allowed the program to bbreak when amount exceeded amount

--- auditor.py
+++ auditor.py
@@ -1,18 +1,31 @@
 #Set Inventory as 0
 inventory = 0
+failed = 0
 
 while True:
     print ("===================")
     print ("Please enter 2 to view current inventory amount")
     print ("Please enter 1 to input your stock quantity")
     print ("Please enter 0 to exit the program")
-    choice = int(input("Enter your Choice here  "))
+    choice = (input("Enter your Choice here  "))
+    if not choice.isdigit():
+        print("Error: Please enter a valid number.")
+        failed += 1
+
+    choice = int(choice)
+
     if choice == 2:
         print (inventory)
 
     if choice == 1:
-        stock = int(input("Please enter the stock amount"))
+        stock = (input("Please enter the stock amount"))
+        stock = int(stock)
         inventory = inventory + stock
 
     if choice == 0:
+        print (inventory)
+        break
+
+    if inventory > 500:
+        print ("Error, Current inventory amount exceeded 500")
         break

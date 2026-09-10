e9996b88d45a22bb4df19a7341dd8ed0b3aef1f9
Yuan Yee <2603843@sit.singaporetech.edu.sg>
2026-09-10 15:17:28 +0800

    Allowed the user to input

---
+++ auditor.py
@@ -0,0 +1,11 @@
+#Set Inventory as 0
+inventory = 0
+
+while True:
+    print ("===================")
+    print ("Please enter 1 to input your stock quantity")
+    print ("Please enter 0 to exit the program")
+    choice = int(input("Enter your Choice here    "))
+
+    if choice == 0:
+        break
